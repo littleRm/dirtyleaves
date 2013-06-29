788adf2489843af142b11e124d7eda5af4c879f8
littleRm <littleRoom@gmail.com>
2013-06-29 13:58:15 -0700

    first commit

---
+++ index.php
@@ -0,0 +1,74 @@
+<?
+	header("Location: blog");
+	exit;
+	
+?><html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<title>dirtyleaves.com</title>
+<style type="text/css">
+<!--
+body {
+	/* Page styling, unrelated to centering */
+	background-color: #1F0000;
+	font-family: Arial, Helvetica, sans-serif;
+	color: #999999;
+	font-size: small;
+	text-align:center;
+}
+
+* { 
+	margin:0;
+	padding:0
+}
+
+/* mac hide \*/
+html, body{
+	height:100%;
+	width:100%;
+	}
+/* end hide */
+
+#outer {
+	height:100%;
+	width:100%;
+	display:table;
+	vertical-align:middle;
+}
+
+#container {
+	display:table-cell;
+	vertical-align:middle;
+}
+
+#inner {
+	text-align: center;
+	margin-left:auto;
+	margin-right:auto;
+}
+
+-->
+</style>
+<!--[if IE ]>
+	<style type="text/css">
+		#container {
+			height: 1px; /* required for IE to properly center vertically */
+           	position:relative;
+			top:50%
+		}
+		#inner {
+			position:relative;
+			top:-50%;
+		}
+	</style>
+<![endif]-->
+</head>
+<body bgcolor="#ffffff">
+<div id="outer">
+  <div id="container">
+    <div id="inner">
+      <a href="/podcasts"><img src="/images/spacer.gif" width="500" height="500" border="0"></a> </div>
+  </div>
+</div>
+</body>
+</html>
